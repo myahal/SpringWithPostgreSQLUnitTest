ef7161c7a0e5a28dbb6a5936e3b913e172c4acb5
Chiharu Miya <miya.chiharu@gmail.com>
2021-08-30 20:27:01 +0900

    Repositoryのテストで別の方法を作ってみた。Controllerのテストを追加

--- src/test/java/com/example/api/CustomerRestControllerTest.java
+++ src/test/java/com/example/api/CustomerRestControllerTest.java
@@ -1,15 +1,40 @@
 package com.example.api;
 
+import com.example.domain.Customer;
+import com.example.service.CustomerService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.equalTo;
 
+@ExtendWith(MockitoExtension.class)
 class CustomerRestControllerTest {
+    @Mock
+    private CustomerService customerService;
+    @InjectMocks
+    private CustomerRestController customerRestController;
+
+    private MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(customerRestController).build();
     }
 
     @AfterEach
@@ -17,6 +42,19 @@ class CustomerRestControllerTest {
     }
 
     @Test
-    void getCustomers() {
+    void getCustomers() throws Exception{
+        List<Customer> customers = List.of(
+                new Customer(1, "Ray", "Ayanami"),
+                new Customer(2, "Asuka", "Langray"),
+                new Customer(3, "Shinji", "Ikari")
+        );
+        Mockito.when(customerRestController.getCustomers()).thenReturn(customers);
+
+        mockMvc.perform(get("/api/customers")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", equalTo(1)))
+                .andExpect(jsonPath("$[0].firstName", equalTo("Ray")))
+                .andExpect(jsonPath("$[0].lastName", equalTo("Ayanami")));
     }
 }
--- src/test/java/com/example/repository/CustomerRepositoryTest2.java
+++ src/test/java/com/example/repository/CustomerRepositoryTest2.java
@@ -6,11 +6,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.util.FileCopyUtils;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,23 +28,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @JdbcTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ContextConfiguration(classes = {CustomerRepository.class})
-class CustomerRepositoryTest {
+class CustomerRepositoryTest2 {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Test
-    @Sql({"/two_users.sql"})
     void findAll_2users() {
+        jdbcTemplate.execute(loadSQL("classpath:two_users.sql"));
         List<Customer> customers = customerRepository.findAll();
         System.out.println(customers);
         assertEquals(2, customers.size());
     }
 
     @Test
-    @Sql({"/three_users.sql"})
     void findAll_3users() {
+        jdbcTemplate.execute(loadSQL("classpath:three_users.sql"));
         List<Customer> customers = customerRepository.findAll();
         System.out.println(customers);
         assertEquals(3, customers.size());
+    }
+
+    private String loadSQL(String fileName) {
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource resource = resourceLoader.getResource(fileName);
+        try ( Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)) {
+            return FileCopyUtils.copyToString(reader);
+        } catch (IOException ex) {
+            throw new UncheckedIOException(ex);
+        }
     }
 }
